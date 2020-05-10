2c3bdd16fb73f75fa0cb12dd9af2a64ac08be789
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-10 21:17:54 +0530

    create new project for polls

---
+++ mypolls/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat unde atque obcaecati laborum optio deserunt vero numquam a ab. Fugit sapiente asperiores doloribus necessitatibus enim quam dolor earum sit quos.</p>
+</main>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
