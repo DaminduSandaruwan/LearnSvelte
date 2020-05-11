a14081be805a65404840f0b4d4b8c8cd7c512270
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-11 23:00:51 +0530

    Add reactive variable(total votes) and do styles

--- mypolls/src/components/PollDetails.svelte
+++ mypolls/src/components/PollDetails.svelte
@@ -1,9 +1,14 @@
 <script>
     export let poll;
+
+    //reactive values
+    $: totalVotes = poll.votesA + poll.votesB;
+
 </script>
 
 <div class="poll">
     <h3>{poll.question}</h3>
+    <p>Total Votes : {totalVotes}</p>
     <div class="answer">
         <div class="percent-a"></div>
         <span>{poll.answerA}({poll.votesA})</span>
@@ -15,5 +20,27 @@
 </div>
 
 <style>
-
+    h3{
+        margin: 0 auto;
+        color: #555;
+    }
+    p{
+        margin-top: 6px;
+        font-size: 14px;
+        color: #aaa;
+        margin-bottom: 30px;
+    }
+    .answer{
+        background: #fafafa;
+        cursor: pointer;
+        margin: 10px auto;
+        position: relative;
+    }
+    .answer:hover{
+        opacity: 0.6;
+    }
+    span{
+        display: inline-block;
+        padding: 10px 20px;
+    }
 </style>
