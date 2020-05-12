7ddaec4f799fd7a66119e19657ad74e47d038bea
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-12 22:34:54 +0530

    lifecycle hooks

--- mypolls/src/components/PollList.svelte
+++ mypolls/src/components/PollList.svelte
@@ -1,9 +1,22 @@
 <script>
+    import {onMount, onDestroy} from 'svelte';
     import PollStore from '../stores/PollStore.js';
     import PollDetails from './PollDetails.svelte';
     export let polls = [];
-    PollStore.subscribe((data)=>{
+
+    const unsub = PollStore.subscribe((data)=>{
         polls = data;
+    });
+
+    onMount(()=>{
+        //maybe get data from a db
+        console.log('component mounted');
+    });
+
+    onDestroy(()=>{
+        //unsub from store
+        console.log('component destroy');
+        unsub();
     });
     
 </script>
